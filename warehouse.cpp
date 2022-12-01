a74d2ad3b838c92855d5fdffa8847af8f1b98322
ElinkaFIT <alena.klimecka@gmail.com>
2022-12-01 17:14:33 +0100

    blbost

--- warehouse.cpp
+++ warehouse.cpp
@@ -7,31 +7,28 @@ int person_ready = 0;
 class Trans : public Process {
     void Behavior() {
         // prepare cycle
-        while(1){
-            // military entry
-            Wait(Exponential(5));
-            // warehouse
-            Enter(warehouse_f,1);
-            Wait(Exponential(5));
-            Leave(warehouse_f,1);
-            // suit up
-            Wait(Exponential(3));
-            person_ready++;
-            break;
-        }
+
+        // military entry
+        Wait(Exponential(5));
+        // warehouse
+        Enter(warehouse_f, 1);
+        Wait(Exponential(5));
+        Leave(warehouse_f, 1);
+        // suit up
+        Wait(Exponential(3));
+        person_ready++;
     };
 };
 
-int main()
-{
+int main() {
     Init(0, 60);
 
     // generate military (300-1500)
-    for (int i=0; i<600; i++){
+    for (int i = 0; i < 600; i++) {
         (new Trans)->Activate();
     }
 
     Run();
     warehouse_f.Output();
-    std::cout<<person_ready;
+    std::cout << person_ready;
 }
